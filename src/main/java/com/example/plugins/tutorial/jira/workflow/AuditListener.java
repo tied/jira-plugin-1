4c2cc045bd1a0e6dc9e6ddd4540048a0a046eeb2
Thomas Epp <Thomas Epp>
2017-02-20 08:02:36 +0100

    Enhanced

---
+++ src/main/java/com/example/plugins/tutorial/jira/workflow/AuditListener.java
@@ -0,0 +1,6 @@
+package com.example.plugins.tutorial.jira.workflow;
+
+
+public class AuditListener {
+}
+
---
+++ src/main/java/com/example/plugins/tutorial/jira/workflow/PopulateFieldsJob.java
@@ -0,0 +1,16 @@
+package com.example.plugins.tutorial.jira.workflow;
+
+import com.atlassian.scheduler.JobRunner;
+import com.atlassian.scheduler.JobRunnerRequest;
+import com.atlassian.scheduler.JobRunnerResponse;
+
+
+public class PopulateFieldsJob implements JobRunner{
+
+	@Override
+	public JobRunnerResponse runJob(JobRunnerRequest request) {
+		return null;
+	}
+
+
+}
