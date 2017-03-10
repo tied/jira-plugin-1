b4e48135940af6eac3dc5cfe61c1afbc6bb2539c
Thomas Epp <thomas.epp.ext@bayer.com>
2017-03-10 13:09:36 +0100

    Field Behaviour

---
+++ src/main/java/de/mtc/jira/wasaut/CSVData.java
@@ -0,0 +1,32 @@
+package de.mtc.jira.wasaut;
+
+import java.util.Map;
+
+public class CSVData {
+
+	private final Map<String, CSVEntry> data;
+	private String changedBy;
+	private String lastChanged;
+	
+	public CSVData(Map<String, CSVEntry> data) {
+		this.data = data;
+	}
+	
+	
+	public String getLastChanged() {
+		return lastChanged;
+	}
+	
+	public void setLastChanged(String lastChanged) {
+		this.lastChanged = lastChanged;
+	}
+	
+	public String getChangedBy() {
+		return changedBy;
+	}
+	
+	public void setChangedBy(String changedBy) {
+		this.changedBy = changedBy;
+	}
+	
+}
